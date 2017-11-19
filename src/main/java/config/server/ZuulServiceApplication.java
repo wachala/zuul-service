ec8565f406b3923a3f61d91b3837421230dc46e2
Piotr Wachala <piotr.wachala@outlook.com>
2017-11-19 14:52:39 +0100

    Add support for configuration service

--- src/main/java/config/server/ZuulServiceApplication.java
+++ src/main/java/config/server/ZuulServiceApplication.java
@@ -3,9 +3,11 @@ package config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class ZuulServiceApplication {
     public static void main(String[] args) {
